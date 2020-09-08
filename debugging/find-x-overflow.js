cf15ea55ac650e096d140e75193daab49741c09b
Luke Perich <43902253+lpke@users.noreply.github.com>
2020-09-08 12:18:14 +1000

    Create find-x-overflow.js

---
+++ debugging/find-x-overflow.js
@@ -0,0 +1,11 @@
+// Paste the below into console to return elements which are causing horizontal overflow
+
+var docWidth = document.documentElement.offsetWidth;
+[].forEach.call(
+  document.querySelectorAll('*'),
+  function(el) {
+    if (el.offsetWidth > docWidth) {
+      console.log(el);
+    }
+  }
+);
